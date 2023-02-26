d25825c92232513e7ac945cd51d4cf858660e1ff
Caroline <wanguic49@gmail.com>
2023-02-26 22:31:20 +0300

    changes to app-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,20 @@ import { HeaderComponent } from './dashboard/header/header.component';
 import { FooterComponent } from './dashboard/footer/footer.component';
 import { AboutUsComponent } from './dashboard/about-us/about-us.component';
 
+import { RouterModule,Routes } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+
+const appRoutes: Routes = [
+  {path: '',redirectTo: 'dashboard' ,pathMatch: 'full'},
+
+  {path:'dashboard',
+  children:[
+    {path:'',redirectTo:'dashboard',pathMatch:'full'},
+    {path:'about-us',component:AboutUsComponent}
+  ]
+}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,9 +30,12 @@ import { AboutUsComponent } from './dashboard/about-us/about-us.component';
     AboutUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    {provide:APP_BASE_HREF,useValue:'/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
